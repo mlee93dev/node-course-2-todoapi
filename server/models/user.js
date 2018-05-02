464685cf323561243113d9e367acfbc005e9eb69
Mark <mark.lee1933@gmail.com>
2018-05-02 19:16:18 +0900

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -91,6 +91,16 @@ UserSchema.statics.findByCredentials = function (email, password){
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  let user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.pre('save', function (next){
   let user = this;
   
--- server/server.js
+++ server/server.js
@@ -131,7 +131,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login (email, password)
 app.post('/users/login', (req, res) => {
   let body = _.pick(req.body, ["email", "password"]);
 
@@ -144,8 +143,18 @@ app.post('/users/login', (req, res) => {
     })
     .catch((e) => {
       res.status(400).send();
+    });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token)
+    .then(() => {
+      res.status(200).send();
     })
-})
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
